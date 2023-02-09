1db4e762c46aac4bf3676f60530e5f69d0be9973
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-02-09 10:14:16 +0100

    adjusts links

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import { mobileMenu } from "./menu-items/mobile-menu.js";
+import { mobileMenu } from "../menu-items/mobile-menu.js";
 
 const mobileMenuBtn = document.querySelector("#mobile-menu-btn");
 mobileMenuBtn.addEventListener("click", mobileMenu);
