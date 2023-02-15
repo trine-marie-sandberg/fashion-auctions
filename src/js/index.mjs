b61c97e1539cb3e608b488f06e36a5993df5da4c
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-02-15 20:04:53 +0100

    test

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,6 +1,5 @@
 import { mobileMenu } from "./menu-items/mobile-menu.js";
 import * as local from "./storage/localstorage.mjs";
-const reuwt = "fashion-auctions"
 const mobileMenuBtn = document.querySelector("#mobile-menu-btn");
 mobileMenuBtn.addEventListener("click", mobileMenu);
 
@@ -22,6 +21,6 @@ loginOutBtn.addEventListener("click", () => {
     else {
         //const originUrl = window.location.origin;
         //document.location.replace(originUrl + "/auth/profile/login");
-        window.location.href = reuwt + "/auth/profile/login/";
+        window.location.href = "/auth/profile/login/";
     }
 });
