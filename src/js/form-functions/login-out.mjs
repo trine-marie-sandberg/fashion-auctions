326794b0f1d0801813245f73c53e2a65b2cd8d92
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-02-19 21:23:47 +0100

    ads relocation on login

--- src/js/form-functions/login-out.mjs
+++ src/js/form-functions/login-out.mjs
@@ -31,16 +31,21 @@ if(registerForm) {
         const data = regiserData(registerForm, event);
         console.log(data);
         handleData(data, registerUrl);
+        window.location.reload();
     });
 };
 
 const loginForm = document.getElementById("login-form");
 if(loginForm) {
-
-    return loginForm.addEventListener("submit", function(event) {
-        const data = loginData(loginForm, event);
-        console.log(data);
-        handleData(data, loginUrl);
+    return loginForm.addEventListener("submit", async (event) => {
+        try {
+            const data = loginData(loginForm, event);
+            await handleData(data, loginUrl);
+            console.log(data);
+            window.location.href = "../index.html"
+        } catch(error) {
+            console.log(error);
+        };
     });
 };
 };
