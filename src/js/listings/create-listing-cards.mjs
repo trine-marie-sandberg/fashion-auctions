ecd0d0504fc3940b8ccb810c0729c2acdb71e26c
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-02-24 23:48:46 +0100

    update styling, remove extra stuff on cards

--- src/js/listings/create-listing-cards.mjs
+++ src/js/listings/create-listing-cards.mjs
@@ -1,7 +1,7 @@
 export function displayPost(listing) {
     //console.log(listing)
     const cardWrap = document.createElement("div");
-    const cardWrapClasses = ["card-wrap", "bg-grey-bg-card", "border-white", "border-radius-xs", "padding-top-btm-med"];
+    const cardWrapClasses = ["card-wrap", "bg-grey-bg-card", "border-white", "border-radius-xs", "padding-top-btm-med", "margin-xs"];
     cardWrap.classList.add(...cardWrapClasses);
     let media = [listing.media];
 
@@ -15,19 +15,20 @@ export function displayPost(listing) {
     const href = "./product-details/?id=" + id;
     cardWrap.innerHTML = `<a class="card even-columns bg-white padding-sm">
                             <div class="listing-info-wrap padding-left-right-sm">
-                            <h2 class="listing-name">${listing.title}</h2>
-                            <p>Created: ${listing.created}</p>
-                            <p>Auction ends: ${listing.endsAt}</p>
-                            <h3>Seller: ${listing.seller.name}</h3>
-                            <p>Description: ${listing.description}</p>
+                              <h2 class="listing-name">${listing.title}</h2>
+                              <p>Auction ends: ${listing.endsAt}</p>
+                              <p>Seller: ${listing.seller.name}</p>
+                              <p>Description: ${listing.description}</p>
                             </div>
                             <div>
                               <img src="" class="listing-img img-auto border-grey-contrast-slight border-radius-sm auto-img" alt="${listing.title}"  onerror="this.onerror=null; this.src='./src/assets/img/placeholder-img.jpg';">
-                              <p>Last updated: ${listing.updated}</p>
                             </div>
+                            <p class="padding-left-right-sm">Tags: ${listing.tags}</p>
                           </a>`;
   
     cardWrap.querySelector("img").src = media;
+    cardWrap.querySelector("img").style.maxHeight = "250px";
+    cardWrap.querySelector("img").style.maxWidth = "350px";
     //cardWrap.querySelector("span").innerText = listing.body;
     cardWrap.querySelector("a").href = href;
     
