9298a11cea20b4add785d6a3df2ba94f8640ce4a
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-02-24 14:04:58 +0100

    relocate about. Ads update avatar form

--- src/js/profile-functions/index.mjs
+++ src/js/profile-functions/index.mjs
@@ -12,13 +12,23 @@ function profileHandler() {
         avatarUrl = "../src/assets/img/avatar-placeholder.png";
     };
 
-    avatarWrap.innerHTML = `<h2 class="margin-xs">Welcome ${userName}</h2>
-                            <div>
-                              <img src="${avatarUrl}" alt="Avatar placeholder" class="avatar bg-white">
-                            </div>`;
+    const htmlAvatar = `<h2 class="margin-xs">Welcome ${userName}</h2>
+                  <div>
+                     <img src="${avatarUrl}" alt="Avatar placeholder" class="avatar bg-white">
+                  </div>`;
+
+    avatarWrap.innerHTML = htmlAvatar;
     console.log(avatarUrl)
-    avatarWrap.addEventListener("click", (event) => {
-        console.log(event)
+
+    const form = document.createElement("form");
+    form.innerHTML = `<div class="bg-white border-grey-contrast-light padding-xs">
+                         <label for="url">Avatar url:</label>
+                         <input type="text" id="url">
+                         <button class="btn bg-primary color-white border-grey-contrast-dark">Submit</button>
+                      </div>`;
+
+    avatarWrap.addEventListener("click", () => {
+        avatarWrap.append(form);
     });
 };
 profileHandler();
